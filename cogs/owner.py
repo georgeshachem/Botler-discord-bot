a6c86e2dbea8a361d6bb943c3c8eb0c9c082b528
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-06-27 14:04:15 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+import discord
+import sys
 
 
 class OwnerCog(commands.Cog, name='Owner Commands', command_attrs={'hidden': True}):
@@ -41,6 +43,52 @@ class OwnerCog(commands.Cog, name='Owner Commands', command_attrs={'hidden': Tru
         if not await ctx.bot.is_owner(ctx.author):
             raise commands.NotOwner('Owner only.')
         return True
+
+    @commands.command(name='join')
+    async def join_voice_channel(self, ctx, *, channel_name: str):
+        """Join a Voice Channel by name"""
+        voice_channels = [x for x in ctx.guild.voice_channels]
+        for channel in voice_channels:
+            if channel_name.lower() in channel.name.lower():
+                await channel.connect()
+
+    @commands.command(name='leave')
+    async def leave_voice_channel(self, ctx, *, channel_name: str):
+        """Leave a Voice Channel"""
+        voice_clients = [x for x in self.bot.voice_clients]
+        for client in voice_clients:
+            if channel_name.lower() in client.channel.name.lower():
+                await client.disconnect()
+
+    @commands.command(aliases=['quit'])
+    async def shutdown(self, ctx):
+        """Shutdown the bot"""
+        await self.bot.logout()
+        sys.exit(0)
+
+    @commands.command(name='game')
+    async def change_game(self, ctx, *, game_name: str):
+        """Change bot's currently playing game name"""
+        await self.bot.change_presence(activity=discord.Game(name=game_name))
+
+    @commands.command(name='status')
+    async def change_status(self, ctx, status: str = ""):
+        """Change bot's status between: offline, idle, dnd and online"""
+        status = status.lower()
+        if status == 'offline' or status == 'off' or status == 'invisible':
+            discordStatus = discord.Status.invisible
+        elif status == 'idle':
+            discordStatus = discord.Status.idle
+        elif status == 'dnd' or status == 'disturb':
+            discordStatus = discord.Status.dnd
+        else:
+            discordStatus = discord.Status.online
+        await self.bot.change_presence(status=discordStatus)
+
+    @commands.command(name='name')
+    async def change_name(self, ctx, *, name = None):
+        """Change bot's name in the guild"""
+        await ctx.guild.me.edit(nick=name)
 
 
 def setup(bot):
